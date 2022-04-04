f719f919ad47b4c7599bac812a6a132ca9e9e1df
AlexBeanie <87626656+AlexBeanie@users.noreply.github.com>
2022-04-03 21:16:28 -0600

    Se agrega el método conversionALtura

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -8,9 +8,12 @@ using namespace std;
 //Para medio decir qué es lo que ocupa una función 
 float tensionCuerda(float masa1, float masa2);
 void eficienciaGasolina (float galon, float millas, float&, float&);
+float conversionAltura(int pies, int pulgadas);
+float conversionMonedas(int libras, int chelines, int peniques);
 
 int main(){
   int numMenu;
+  //despliega el menu de selección
   while(numMenu != 7){
     cout<<"\n\nEjercicios de ejecucion de metodos:\n";
     cout<<"1. Maquina de Atwood.\n2.Eficiencia de Gasolina.\n3.Conversion de altura.\n4.Conversion monetaria britanica.\n5.Numeros aleatorios.\n6.Numeros primos.\n7.Salir";
@@ -38,12 +41,21 @@ int main(){
         eficienciaGM = 0;
         eficienciaLK = 0;
         eficienciaGasolina(galon, millas, eficienciaGM, eficienciaLK);
-        cout<<"La eficiencia del carro es: "<< eficienciaGM <<"mi/gal" ;
-        cout<<"equivalente a "<< eficienciaLK<<" l/km";
+        cout<<"La eficiencia del carro es: "<< eficienciaGM <<" mi/gal.\n" ;
+        cout<<"Equivalente a "<< eficienciaLK<<" l/km.";
         break;
       
       case 3:
-        cout<<numMenu+1;
+        int pies, pulgadas;
+        float alturaMetros;
+        cout<<"Ingrese el numero de pies de altura:";
+        cin>>pies;
+        cout<<"Ingrese el numero de pulgadas de altura:";
+        cin>>pulgadas;
+        alturaMetros = conversionAltura(pies,pulgadas);
+        cout<<"Altura: "<<pies<<" pies "<<pulgadas<<" pulgadas.";
+        cout<<"La altura de la persona es: "<<alturaMetros<<" m";
+
         break;
 
       case 4:
@@ -75,8 +87,14 @@ float tensionCuerda(float masa1, float masa2)
 void eficienciaGasolina(float galon, float millas, float& eficienciaGM, float& eficienciaLK)
 {
   float kilometros, litros;
-  kilometros = millas*1,609;
-  litros= galon*3,785;
+  kilometros = millas*1.609;
+  litros= galon*3.785;
   eficienciaGM = galon/millas;
   eficienciaLK = litros/kilometros;
+}
+
+float conversionAltura(int pies, int pulgadas)
+{
+  float alturaMetros = (pies/3.281) + (pulgadas/39.37);
+  return alturaMetros;
 }
